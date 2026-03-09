f459d8b472e0c260a0868826d771d73a2092e5f9
PatrykEmilLibert <libert.patryk@gmail.com>
2026-03-09 13:31:32 +0100

    release: bump version to 6.0.20

--- src/App.tsx
+++ src/App.tsx
@@ -95,12 +95,12 @@ export default function App() {
   const [showGitHubAuth, setShowGitHubAuth] = useState(false);
   const [isWindowsFrameless, setIsWindowsFrameless] = useState(false);
   const [isWindowMaximized, setIsWindowMaximized] = useState(false);
-  const [appVersion, setAppVersion] = useState<string>("6.0.19");
+  const [appVersion, setAppVersion] = useState<string>("6.0.20");
 
   useEffect(() => {
     getVersion()
       .then(setAppVersion)
-      .catch(() => setAppVersion("6.0.19"));
+      .catch(() => setAppVersion("6.0.20"));
   }, []);
 
   useEffect(() => {
