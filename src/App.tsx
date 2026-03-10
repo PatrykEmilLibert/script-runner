001fb9145bc8505cc36b5f21f21b4d66101d52f9
PatrykEmilLibert <libert.patryk@gmail.com>
2026-03-10 10:01:13 +0100

    chore: bump version to 6.0.29

--- src/App.tsx
+++ src/App.tsx
@@ -95,12 +95,12 @@ export default function App() {
   const [showGitHubAuth, setShowGitHubAuth] = useState(false);
   const [isWindowsFrameless, setIsWindowsFrameless] = useState(false);
   const [isWindowMaximized, setIsWindowMaximized] = useState(false);
-  const [appVersion, setAppVersion] = useState<string>("6.0.28");
+  const [appVersion, setAppVersion] = useState<string>("6.0.29");
 
   useEffect(() => {
     getVersion()
       .then(setAppVersion)
-      .catch(() => setAppVersion("6.0.28"));
+      .catch(() => setAppVersion("6.0.29"));
   }, []);
 
   useEffect(() => {
