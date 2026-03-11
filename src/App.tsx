02cd3d4b9a64f589ada3161a910e5a53ddd09185
PatrykEmilLibert <libert.patryk@gmail.com>
2026-03-11 10:06:22 +0100

    chore(release): bump version to v6.0.41

--- src/App.tsx
+++ src/App.tsx
@@ -95,13 +95,13 @@ export default function App() {
   const [showGitHubAuth, setShowGitHubAuth] = useState(false);
   const [isWindowsFrameless, setIsWindowsFrameless] = useState(false);
   const [isWindowMaximized, setIsWindowMaximized] = useState(false);
-  const [appVersion, setAppVersion] = useState<string>("6.0.40");
+  const [appVersion, setAppVersion] = useState<string>("6.0.41");
   const hasBootstrappedRef = useRef(false);
 
   useEffect(() => {
     getVersion()
       .then(setAppVersion)
-      .catch(() => setAppVersion("6.0.40"));
+      .catch(() => setAppVersion("6.0.41"));
   }, []);
 
   useEffect(() => {
